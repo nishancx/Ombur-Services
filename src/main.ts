d6e45c5604903cd66e240f4f5f399c1161d1b71d
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-09-11 23:15:34 +0545

    feat: allow authorization

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,9 @@ import { jsonParse } from './utils/object.js'
 const app = express()
 app.use(cookieParser())
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*') // Adjust origin as needed
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', 'https://ombur.vercel.app')
+  res.header('Access-Control-Allow-Headers', 'Authorization')
   next()
 })
 
