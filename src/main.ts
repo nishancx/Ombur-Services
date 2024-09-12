4a971a49c1c03710c707a0fb749503a55a0dacd6
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2024-09-12 19:14:50 +0545

    feat: allow credentials

--- src/main.ts
+++ src/main.ts
@@ -32,6 +32,7 @@ app.use(
 )
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Credentials', 'true')
   res.cookie('__Secure-authjs.session-token', 'test', {
     sameSite: 'none',
   })
